937a162bc4175dc6e686618a01ffa2b39a658c88
Ayush <ayushrajput000031@gmail.com>
2024-10-07 12:20:04 +0530

    added check odd or even

---
+++ index.js
@@ -0,0 +1,10 @@
+let array = [ 3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18]
+for(i=0;i<array.length;i++){
+    if(array[i]%2==0){
+        console.log(array[i],"is a even number");
+    }
+    else{
+     console.log(array[i],"is a odd number");
+    }
+    
+}
